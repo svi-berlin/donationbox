21076e4fcb13cfa66eeb3a64e85601e647899e47
root <root@raspberrypi.(none)>
2014-10-04 23:49:16 +0200

    launcher script called by cronjob @reboot

---
+++ init-donationbox.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /usr/local/bin/donationbox/
+sudo python donationbox.py
+cd /home/pi/
+
